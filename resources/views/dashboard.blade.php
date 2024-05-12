b9b62708125272c926dc6ba78471a433fd695a31
Anton Plauche <antonplauche92@gmail.com>
2024-05-12 13:16:19 -0500

    add confirmation to website deleting

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -38,10 +38,10 @@
                         <a class="opacity-50 hover:opacity-100" href="{{ route('websites.edit', $website) }}">
                             <svg width="24px" height="24px" viewBox="0 0 24 24" stroke-width="1" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M20 12V5.74853C20 5.5894 19.9368 5.43679 19.8243 5.32426L16.6757 2.17574C16.5632 2.06321 16.4106 2 16.2515 2H4.6C4.26863 2 4 2.26863 4 2.6V21.4C4 21.7314 4.26863 22 4.6 22H11" stroke="#000000" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path><path d="M8 10H16M8 6H12M8 14H11" stroke="#000000" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path><path d="M17.9541 16.9394L18.9541 15.9394C19.392 15.5015 20.102 15.5015 20.5399 15.9394V15.9394C20.9778 16.3773 20.9778 17.0873 20.5399 17.5252L19.5399 18.5252M17.9541 16.9394L14.963 19.9305C14.8131 20.0804 14.7147 20.2741 14.6821 20.4835L14.4394 22.0399L15.9957 21.7973C16.2052 21.7646 16.3988 21.6662 16.5487 21.5163L19.5399 18.5252M17.9541 16.9394L19.5399 18.5252" stroke="#000000" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path><path d="M16 2V5.4C16 5.73137 16.2686 6 16.6 6H20" stroke="#000000" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path></svg>
                         </a>
-                        <form action="{{ route('websites.destroy', $website) }}" method="POST">
+                        <form id="deleteWebsiteForm" action="{{ route('websites.destroy', $website) }}" method="POST">
                             @method('DELETE')
                             @csrf
-                            <button class="opacity-50 hover:opacity-100" type="submit">
+                            <button class="opacity-50 hover:opacity-100" type="submit" onclick="return confirmSubmit()">
                                 <svg width="24px" height="24px" viewBox="0 0 24 24" stroke-width="1" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M20 9L18.005 20.3463C17.8369 21.3026 17.0062 22 16.0353 22H7.96474C6.99379 22 6.1631 21.3026 5.99496 20.3463L4 9" stroke="#000000" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path><path d="M21 6L15.375 6M3 6L8.625 6M8.625 6V4C8.625 2.89543 9.52043 2 10.625 2H13.375C14.4796 2 15.375 2.89543 15.375 4V6M8.625 6L15.375 6" stroke="#000000" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path></svg>
                             </button>
                         </form>
@@ -56,4 +56,17 @@
 
         </div>
     </div>
+
+
+
+    <script>
+        function confirmSubmit() {
+            if (confirm('Are you sure you want to delete this site?')) {
+                document.getElementById('deleteWebsiteForm').submit();
+                return true;
+            }
+            return false;
+        }
+    </script>
 </x-app-layout>
+
